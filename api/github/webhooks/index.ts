71834a6c9944aaf1010d9eb90945b7013542dd78
Logan Anderson <landerson2@upei.ca>
2023-07-05 09:41:04 -0400

    try edge runtime

--- api/github/webhooks/index.ts
+++ api/github/webhooks/index.ts
@@ -2,6 +2,10 @@
 // This is used for hosting the app on Vercel
 import { createNodeMiddleware, createProbot } from "probot";
 
+export const config = {
+  runtime: "edge",
+};
+
 import { app } from "../../../src/app";
 
 module.exports = createNodeMiddleware(app, {
