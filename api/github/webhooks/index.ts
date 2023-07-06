10fe38b136ba0f3c2b156119d9ef08f0444b2ca2
Logan Anderson <landerson2@upei.ca>
2023-07-06 11:17:12 -0400

    remove edge runtime

--- api/github/webhooks/index.ts
+++ api/github/webhooks/index.ts
@@ -2,10 +2,6 @@
 // This is used for hosting the app on Vercel
 import { createNodeMiddleware, createProbot } from "probot";
 
-export const config = {
-  runtime: "edge",
-};
-
 import { app } from "../../../src/app";
 
 module.exports = createNodeMiddleware(app, {
