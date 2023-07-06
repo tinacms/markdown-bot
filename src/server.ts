bd3eab5251bab535cc67e47b6d0e3881abb64c5b
Logan Anderson <landerson2@upei.ca>
2023-07-06 14:33:33 -0400

    remove log

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,17 @@ import { app } from "./app";
 
 config();
 
+const key = process.env.RENDER
+  ? process.env.PRIVATE_KEY?.replace(/\\n/g, "\n")
+  : process.env.PRIVATE_KEY;
 const server = new Server({
   webhookProxy: process.env.WEBHOOK_PROXY_URL,
   Probot: Probot.defaults({
     appId: process.env.APP_ID,
-    privateKey: process.env.PRIVATE_KEY,
+    privateKey: key,
     secret: process.env.WEBHOOK_SECRET,
   }),
 });
 server.load(app).then(() => {
-  console.log("key", process.env.PRIVATE_KEY);
   server.start();
 });
