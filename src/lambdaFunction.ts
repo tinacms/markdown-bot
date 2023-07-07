c99530e202526de3ebd039b0c228d5c42d2a3b50
Logan Anderson <landerson2@upei.ca>
2023-07-07 10:42:02 -0400

    remove unused code

--- src/lambdaFunction.ts
+++
@@ -1,9 +0,0 @@
-import {
-  createLambdaFunction,
-  createProbot,
-} from "@probot/adapter-aws-lambda-serverless";
-import { app } from "./app";
-
-export const handler = createLambdaFunction(app, {
-  probot: createProbot(),
-});
